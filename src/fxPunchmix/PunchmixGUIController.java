8d11d45e95a61d5e5f4e36d013ed5af6961a69c3
Zualix <otsy.97@hotmail.com>
2022-11-12 19:01:55 +0200

    Added first handler and deleted modulo-info

--- src/fxPunchmix/PunchmixGUIController.java
+++ src/fxPunchmix/PunchmixGUIController.java
@@ -1,5 +1,19 @@
 package fxPunchmix;
 
+import fi.jyu.mit.fxgui.Dialogs;
+import javafx.fxml.FXML;
+
+
 public class PunchmixGUIController {
-	
+    @FXML
+    void handleSave() {
+        save();
+    }
+    
+    
+    //=====================================
+    
+    private void save() {
+        Dialogs.showMessageDialog("Should save! Not done yet!");
+    }
 }
--- src/module-info.java
+++
@@ -1,6 +0,0 @@
-module ht {
-	requires javafx.controls;
-	requires javafx.fxml;
-	
-	opens fxPunchmix to javafx.graphics, javafx.fxml;
-}
