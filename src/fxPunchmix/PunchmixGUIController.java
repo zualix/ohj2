83e7cb286fabcdaf49983414daf7ba7e7c4f93af
Zualix <otsy.97@hotmail.com>
2022-11-13 18:43:06 +0200

    added all handlers for the punchmixguiview and did those for the controller aswell

--- src/fxPunchmix/PunchmixGUIController.java
+++ src/fxPunchmix/PunchmixGUIController.java
@@ -15,11 +15,71 @@ public class PunchmixGUIController {
     void handleEditDrink() {
         ModalController.showModal(PunchmixGUIController.class.getResource("DrinkDialogView.fxml"), "Drink", null, "");
     }
+    @FXML
+    void handleAbout() {
+        about();
+    }
+
+    @FXML
+    void handleClose() {
+        close();
+    }
+
+    @FXML
+    void handleDel() {
+        delete();
+    }
+
+    @FXML
+    void handleEditIng() {
+        ModalController.showModal(PunchmixGUIController.class.getResource("IngDialogView.fxml"), "Ingredient", null, "");
+    }
+
+    @FXML
+    void handleNewDrink() {
+        ModalController.showModal(PunchmixGUIController.class.getResource("DrinkDialogView.fxml"), "Drink", null, "");
+    }
+
+    @FXML
+    void handleNewIng() {
+        ModalController.showModal(PunchmixGUIController.class.getResource("NewIngDialogView.fxml"), "Ingredient", null, "");
+    }
+
+    @FXML
+    void handlePrint() {
+        print();
+    }
+
+    @FXML
+    void handleSearch() {
+        search();
+    }
     
-    
-    //=====================================
+    //===================================== 
     
     private void save() {
         Dialogs.showMessageDialog("Should save! Not done yet!");
     }
+    
+    private void about() {
+        Dialogs.showMessageDialog("Not done yet!");
+    }
+    
+    private void close() {
+        Dialogs.showMessageDialog("Not done yet!");
+    }
+    
+    private void delete() {
+        Dialogs.showMessageDialog("Not done yet!");
+    }
+    
+    private void print() {
+        Dialogs.showMessageDialog("Not done yet!");
+    }
+    
+    private void search() {
+        Dialogs.showMessageDialog("Not done yet!");
+    }
+    
+    
 }
