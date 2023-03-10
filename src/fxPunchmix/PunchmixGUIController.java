1fb04c8f40f9b8f034213a702d2d482025b09b2f
Zualix <otsy.97@hotmail.com>
2023-03-10 18:15:40 +0200

    Added step 5.1 text to handle opening and closing aswell start of the search funktion

--- src/fxPunchmix/PunchmixGUIController.java
+++ src/fxPunchmix/PunchmixGUIController.java
@@ -1,19 +1,48 @@
 package fxPunchmix;
 
 import java.io.IOException;
+import java.io.PrintStream;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import fi.jyu.mit.fxgui.Dialogs;
+import fi.jyu.mit.fxgui.ListChooser;
 import fi.jyu.mit.fxgui.ModalController;
+import fi.jyu.mit.fxgui.TextAreaOutputStream;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import punchmix.Drink;
+import punchmix.Ingredient;
+import punchmix.Mix;
+import punchmix.Punchmix;
+import side.*;
 
 
-public class PunchmixGUIController {
+public class PunchmixGUIController implements Initializable{
+	
+
+    @FXML private ListChooser<Drink> chooserDrink;
+    @FXML private Label labelError;
+    @FXML private TextField searcCrit;
+    @FXML private ScrollPane panelDrink;
+	
+    @Override
+    public void initialize(URL url, ResourceBundle bundle) {
+        start();      
+    }
+
+    
     @FXML
     void handleSave() {
         save();
@@ -56,18 +85,55 @@ public class PunchmixGUIController {
 
     @FXML
     void handlePrint() {
-        print();
+       // TulostusController tulostusCtrl = TulostusController.tulosta(null); 
+       //tulostaValitut(tulostusCtrl.getTextArea()); 
+
     }
 
     @FXML
     void handleSearch() {
-        search();
+        //search();
+        String crit = searcCrit.getText(); 
+        if ( crit.isEmpty() )
+            showError(null);
+        else
+        	showError("Ei osata vielä hakea : " + crit);
+
     }
     
     //===========================================================================================================
+    private Punchmix punch;
+    private Drink drinkPos;
+    private TextArea areaDrink = new TextArea();
+    
+   
+    protected void start() {
+        panelDrink.setContent(areaDrink);
+        areaDrink.setFont(new Font("Courier New", 12));
+        panelDrink.setFitToHeight(true);
+        
+        chooserDrink.clear();
+        chooserDrink.addSelectionListener(e -> showDrink());
+    }
+
+    private void showError(String err) {
+        if ( err == null || err.isEmpty() ) {
+        	labelError.setText("");
+        	labelError.getStyleClass().removeAll("err");
+            return;
+        }
+        labelError.setText(err);
+        labelError.getStyleClass().add("err");
+    }
+    
     
     private void save() {
         Dialogs.showMessageDialog("Should save! Not done yet!");
+    }
+   
+    public boolean canClose() {
+    	save();
+    	return true;
     }
     
     private void about() {
@@ -82,12 +148,69 @@ public class PunchmixGUIController {
         Dialogs.showMessageDialog("Not done yet!");
     }
     
-    private void print() {
-        Dialogs.showMessageDialog("Not done yet!");
+    
+
+    protected void showDrink() {
+    	drinkPos = chooserDrink.getSelectedObject();
+    	
+    	if (drinkPos == null) return;
+    	
+    	areaDrink.setText("");
+    	try (PrintStream os = TextAreaOutputStream.getTextPrintStream(areaDrink)) {
+    		drinkPos.print(os);
+    	}
     }
     
-    private void search() {
-        Dialogs.showMessageDialog("Not done yet!");
+    public void print(PrintStream os, final Drink drink) {
+        os.println("----------------------------------------------");
+        drink.print(os);
+        os.println("----------------------------------------------");
+
+    }
+    
+
+    public void printSelected(TextArea text) {
+        try (PrintStream os = TextAreaOutputStream.getTextPrintStream(text)) {
+            os.println("Printing all the drinks");
+            for (int i = 0; i < punch.getDrinks(); i++) {
+                Drink drink =  punch.giveDrink(i);
+                print(os, drink);
+                os.println("\n\n");
+            }
+        }
+    }
+    
+    private void search(int idd) {
+    	chooserDrink.clear();
+    	
+    	int index = 0;
+    	for (int i = 0; i < punch.getDrinks(); i++) {
+    		Drink drink = punch.giveDrink(i);
+    		if (drink.getIdd() == idd) index = i;
+    		chooserDrink.add(drink.getName(), drink);
+    	}
+    	chooserDrink.setSelectedIndex(index);
+    }
+    
+    
+    
+    protected void newDrinkEx() {
+    	Drink extra = new Drink();
+    	extra.recIdd();
+    	extra.someMix();
+    	
+    	try {
+    		punch.addMix(extra);
+    	} catch (SailoException e) {
+			Dialogs.showMessageDialog("Problem to crate new " + e.getMessage());
+			return;
+		}
+    	search(extra.getIdd());
+    }
+    
+    public void setPunch(Punchmix punch){
+    	this.punch = punch;
+    	showDrink();
     }
     
     private void newIng() {
--- src/fxPunchmix/PunchmixMain.java
+++ src/fxPunchmix/PunchmixMain.java
@@ -1,9 +1,12 @@
 package fxPunchmix;
 	
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.stage.Stage;
+import punchmix.Punchmix;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.fxml.FXMLLoader;
 
 
@@ -12,11 +15,27 @@ public class PunchmixMain extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("PunchmixGUIView.fxml"));
-			Scene scene = new Scene(root,850,400);
+			
+			final FXMLLoader ldr = new FXMLLoader(getClass().getResource("PunchmixGUIView.fxml"));
+			final Pane root = (Pane)ldr.load();
+			final PunchmixGUIController punchCtrl = (PunchmixGUIController)ldr.getController();
+			
+			final Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("punchmix.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
+			//BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("PunchmixGUIView.fxml"));
+			//Scene scene = new Scene(root,850,400);
+			//scene.getStylesheets().add(getClass().getResource("punchmix.css").toExternalForm());
+			
+			primaryStage.setOnCloseRequest((event) -> {
+				if ( !punchCtrl.canClose() ) event.consume();
+			});
+			
+			Punchmix punch = new Punchmix();
+			punchCtrl.setPunch(punch);
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
